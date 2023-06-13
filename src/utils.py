22356a0037d8a0d13450987a186d74991b89731d
KonradIT <github@chernowii.com>
2023-06-13 21:03:23 +0200

    Allow custom dates to be passed

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
 from datetime import date, timedelta
+import os
 
 
 def get_day(days: int = None) -> str:
+    if os.getenv("INPUT_CUSTOM_DATE") != None and os.getenv("INPUT_CUSTOM_DATE") != "":
+        return str(os.getenv("INPUT_CUSTOM_DATE"))
     today = date.today()
     if days != None:
         today = today - timedelta(days=days)
